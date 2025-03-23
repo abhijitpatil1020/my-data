700e6a673e6d04121dfac4eb8e647d2e9488ebb5
abhijitpatil1020 <abhijeetp821@gmail.com>
2025-03-23 15:59:33 +0530

    Delete web.sh

--- web.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-apt update
-apt install wget unzip apache2 -y
-systemctl start apache2
-systemctl enable apache2
-wget https://www.tooplate.com/zip-templates/2079_garage.zip
-unzip -o 2079_garage.zip
-cp -r 2079_garage/* /var/www/html/
-systemctl restart apache2
